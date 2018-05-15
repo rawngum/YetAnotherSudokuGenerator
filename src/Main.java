af26e0fbe890ac1329f89777eccdcfb3a9728865
rwangum <swan.benoit@gmail.com>
2018-05-15 10:42:30 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+//	    System.out.println("Hello World!");
+//	    Board board = Board (); 
+//	   board.import ("fichier sudoku type csv");
+//    board.solve();
+//	    board.print();
 	}
 
 }
