30d396a741385ace8d23551f4bc4d9d07b30ae56
rawngum <swan.benoit@gmail.com>
2018-06-01 16:07:40 +0200

    ajustement des droits des methodes

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	    Generator sudoku = new Generator();
 	    sudoku.makeBoard(1);
 		System.out.println(sudoku);
-		System.out.println(sudoku.howManyNumbers());
+		System.out.println(sudoku.howManyNumbers() + " holes were made");
 	    Solver.solveBoard(sudoku);
 		System.out.println(sudoku);
 	}
